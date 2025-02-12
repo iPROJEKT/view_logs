b07a1bf56bf0f7d1fce1dca00145f798fbc91998
Your Name <@triniti008.momentum.loc>
2025-02-12 07:43:19 +0300

    maybe fix all

--- test.py
+++ test.py
@@ -1,24 +1,48 @@
 from direct.showbase.ShowBase import ShowBase
-from direct.task.TaskManagerGlobal import taskMgr
-from panda3d.core import CardMaker, NodePath
+from direct.gui.DirectGui import DirectOptionMenu, DirectFrame, DGG
+from panda3d.core import TextNode
 
 
-class fff(ShowBase):
+class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        logo_texture = loader.loadTexture("static/img/trinititech.jpg")
-        cm = CardMaker("splash")
-        cm.setFrame(-1, 1, -1, 1)
-        splash = NodePath(cm.generate())
-        splash.setTexture(logo_texture)
-        splash.reparentTo(self.render2d)
-        self.hideSplashTask = taskMgr.doMethodLater(3, self.hide_splash, "hideSplashTask")
 
-    def hide_splash(self, task):
-        self.render2d.hide()
-        print("Заставка скрыта, загружаем основное приложение...")
-        return task.done
+        # Создаем фрейм (окно)
+        self.info_frame = DirectFrame(
+            frameColor=(46 / 255, 46 / 255, 46 / 255, 1),
+            frameSize=(-0.5, 0.7, -0.1, 0.4),
+            pos=(-0.8, 0, -0.8),
+        )
+        self.filter_frame = DirectFrame(
+            frameColor=(46 / 255, 200 / 255, 46 / 255, 1),
+            frameSize=(-0.18, 0.18, -0.06, 0.06),
+            pos=(0.4, 0, 0.18),
+            parent=self.info_frame
+        )
+        self.value_frame = DirectFrame(
+            frameColor=(46 / 255, 100 / 255, 46 / 255, 1),
+            frameSize=(-0.18, 0.18, -0.06, 0.06),
+            pos=(0.4, 0, 0.31),
+            parent=self.info_frame
+        )
+
+        # Создаем DirectOptionMenu
+        self.magnitude_menu = DirectOptionMenu(
+            text="Величина",
+            items=["I", "U", "WFS"],
+            pos=(-0.02, 0, 0),
+            scale=0.1,
+            frameSize=(-1.7, 1.7, -0.65, 0.65),
+            text_pos=(0.15, -0.3),
+            relief=None,
+            popupMarker_relief=None,
+            text_align=TextNode.ACenter,
+            item_relief=DGG.FLAT,
+            item_text_fg=(1, 1, 1, 1),
+            item_frameColor=(0.3, 0.3, 0.3, 1),
+        )
 
 
-app = fff()
-app.run()
+if __name__ == '__main__':
+    app = MyApp()
+    app.run()
